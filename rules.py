17e0f500233f47d6fedfee087a1d1ab2c2e876ea
Yunyang <yunyang5@ualberta.ca>
2023-11-27 14:48:29 -0700

    bug fix

--- rules.py
+++ rules.py
@@ -9,6 +9,28 @@ fire
 
 #Declare each fuzzy rule
 [
+    ctrl.Rule(bullet_delta['M'], Fire['H']),
+    ctrl.Rule(not bullet_delta['M'], Fire['L']),
+
+    ctrl.Rule(collision_time['L'] & evade_delta['H'], (thrust ['L'], turn['M'])),
+    ctrl.Rule(collision_time['L'] & evade_delta['L'], (thrust ['L'], turn['M'])),
+    ctrl.Rule(collision_time['L'] & evade_delta['ML'], (thrust ['MH'], turn['ML'])),
+    ctrl.Rule(collision_time['L'] & evade_delta['MH'], (thrust ['MH'], turn['MH'])),
+    ctrl.Rule(collision_time['L'] & evade_delta['M'], (thrust ['H'], turn['M'])),    
+    
+    ctrl.Rule(collision_time['M'] & evade_delta['H'], (thrust ['ML'], turn['M'])),
+    ctrl.Rule(collision_time['M'] & evade_delta['L'], (thrust ['ML'], turn['M'])),
+    ctrl.Rule(collision_time['M'] & evade_delta['ML'], (thrust ['M'], turn['ML'])),
+    ctrl.Rule(collision_time['M'] & evade_delta['MH'], (thrust ['M'], turn['MH'])),
+    ctrl.Rule(collision_time['M'] & evade_delta['M'], (thrust ['MH'], turn['M'])),
+
+    ctrl.Rule(collision_time['H'] & evade_delta['H'], (thrust ['ML'], turn['H'])),
+    ctrl.Rule(collision_time['H'] & evade_delta['L'], (thrust ['ML'], turn['L'])),
+    ctrl.Rule(collision_time['H'] & evade_delta['ML'], (thrust ['H'], turn['ML'])),
+    ctrl.Rule(collision_time['H'] & evade_delta['MH'], (thrust ['H'], turn['MH'])),
+    ctrl.Rule(collision_time['H'] & evade_delta['M'], (thrust ['H'], turn['M'])),
+
+
     ctrl.Rule(collision_time['L'] & evade_delta['H'], (thrust ['L'], turn['M'])),
     ctrl.Rule(collision_time['L'] & evade_delta['L'], (thrust ['L'], turn['M'])),
     ctrl.Rule(collision_time['L'] & evade_delta['ML'], (thrust ['MH'], turn['ML'])),
