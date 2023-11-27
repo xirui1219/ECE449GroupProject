0cdfaaea7549f3d7c64fe74d808b9be6737867d9
Yunyang <yunyang5@ualberta.ca>
2023-11-26 23:39:10 -0700

    target_angle method added

--- target_angle.py
+++ target_angle.py
@@ -1,5 +1,7 @@
 import math
 
+debug = False
+
 def closest_asteroid (ship_state, game_state):
 
     ship_pos_x = ship_state["position"][0] 
@@ -36,11 +38,11 @@ def closest_asteroid (ship_state, game_state):
                         closest_3_asteroid[i]["aster"] = a 
                         closest_3_asteroid[i]["dist"] = curr_dist
                         break
-
-    print (str(closest_3_asteroid[0]["dist"]))
-    print (str(closest_3_asteroid[1]["dist"]))
-    print (str(closest_3_asteroid[2]["dist"]))
-    print()
+    if debug :
+        print (str(closest_3_asteroid[0]["dist"]))
+        print (str(closest_3_asteroid[1]["dist"]))
+        print (str(closest_3_asteroid[2]["dist"]))
+        print()
     return closest_3_asteroid
 
 # return the smallest asteroid or the closest one if size ties
@@ -59,8 +61,64 @@ def smallest_asteroid (closest_3_asteroid):
             if asteroid_dict["dist"] < smallest_asteroid["dist"]:
                 smallest_asteroid = asteroid_dict
 
-    return smallest_asteroid["aster"]
+    return smallest_asteroid
+
 
 # method for estimate angle of target
 def target_angle (ship_state, target_asteroid):
-    return
+
+    # Find the closest asteroid (disregards asteroid velocity)
+    ship_pos_x = ship_state["position"][0]     # See src/kesslergame/ship.py in the KesslerGame Github
+    ship_pos_y = ship_state["position"][1]       
+
+
+    # closest_asteroid is now the nearest asteroid object. 
+    # Calculate intercept time given ship & asteroid position, asteroid velocity vector, bullet speed (not direction).
+    # Based on Law of Cosines calculation, see notes.
+    
+    
+    asteroid_ship_x = ship_pos_x - target_asteroid["aster"]["position"][0]
+    asteroid_ship_y = ship_pos_y - target_asteroid["aster"]["position"][1]
+    
+    asteroid_ship_theta = math.atan2(asteroid_ship_y,asteroid_ship_x)
+    
+    asteroid_direction = math.atan2(target_asteroid["aster"]["velocity"][1], target_asteroid["aster"]["velocity"][0]) # Velocity is a 2-element array [vx,vy].
+    my_theta2 = asteroid_ship_theta - asteroid_direction
+    cos_my_theta2 = math.cos(my_theta2)
+    # Need the speeds of the asteroid and bullet. speed * time is distance to the intercept point
+    asteroid_vel = math.sqrt(target_asteroid["aster"]["velocity"][0]**2 + target_asteroid["aster"]["velocity"][1]**2)
+    bullet_speed = 800 # Hard-coded bullet speed from bullet.py
+    
+    # Determinant of the quadratic formula b^2-4ac
+    targ_det = (-2 * target_asteroid["dist"] * asteroid_vel * cos_my_theta2)**2 - (4*(asteroid_vel**2 - bullet_speed**2) * target_asteroid["dist"])
+    
+    # Combine the Law of Cosines with the quadratic formula for solve for intercept time. Remember, there are two values produced.
+    intrcpt1 = ((2 * target_asteroid["dist"] * asteroid_vel * cos_my_theta2) + math.sqrt(targ_det)) / (2 * (asteroid_vel**2 -bullet_speed**2))
+    intrcpt2 = ((2 * target_asteroid["dist"] * asteroid_vel * cos_my_theta2) - math.sqrt(targ_det)) / (2 * (asteroid_vel**2-bullet_speed**2))
+    
+    # Take the smaller intercept time, as long as it is positive; if not, take the larger one.
+    if intrcpt1 > intrcpt2:
+        if intrcpt2 >= 0:
+            bullet_t = intrcpt2
+        else:
+            bullet_t = intrcpt1
+    else:
+        if intrcpt1 >= 0:
+            bullet_t = intrcpt1
+        else:
+            bullet_t = intrcpt2
+            
+    # Calculate the intercept point. The work backwards to find the ship's firing angle my_theta1.
+    intrcpt_x = target_asteroid["aster"]["position"][0] + target_asteroid["aster"]["velocity"][0] * bullet_t
+    intrcpt_y = target_asteroid["aster"]["position"][1] + target_asteroid["aster"]["velocity"][1] * bullet_t
+    
+    my_theta1 = math.atan2((intrcpt_y - ship_pos_y),(intrcpt_x - ship_pos_x))
+    
+    # Lastly, find the difference betwwen firing angle and the ship's current orientation. BUT THE SHIP HEADING IS IN DEGREES.
+    shooting_theta = my_theta1 - ((math.pi/180)*ship_state["heading"])
+    
+    # Wrap all angles to (-pi, pi)
+    shooting_theta = (shooting_theta + math.pi) % (2 * math.pi) - math.pi
+    
+    print (shooting_theta)
+    return shooting_theta
